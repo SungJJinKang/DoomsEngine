2914df6104959c7f3f98e56f028f61a5332dcdbd
SungJinKang <hour30000@gmail.com>
2021-08-24 16:57:31 +0900

    fix bugs

--- Doom3/Source/Core/utility/BVH/BVH_Node_Object.cpp
+++ Doom3/Source/Core/utility/BVH/BVH_Node_Object.cpp
@@ -3,9 +3,9 @@
 template <typename ColliderType>
 void doom::BVH_Node_Object<ColliderType>::UpdateBVH_Node()
 {
-	if (IsWorldColliderCacheDirty.GetIsDirty(false) == true)
+	if (doom::ColliderUpdater<ColliderType>::IsWorldColliderCacheDirty.GetIsDirty(false) == true)
 	{//Only when WorldAABB is updated,
-		doom::BVH_Node_Container<ColliderType>::UpdateBVH_Node(*(GetWorldColliderCacheByReference()));
+		doom::BVH_Node_Container<ColliderType>::UpdateBVH_Node(*(doom::ColliderUpdater<ColliderType>::GetWorldColliderCacheByReference()));
 	}
 	
 }
