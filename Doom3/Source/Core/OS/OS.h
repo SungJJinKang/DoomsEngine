f7b691b2e1f9e91dfb511dbef900e2157a4c79c2
SungJinKang <hour30000@gmail.com>
2021-09-13 22:17:50 +0900

    add thread affinity functions

--- Doom3/Source/Core/OS/OS.h
+++ Doom3/Source/Core/OS/OS.h
@@ -2,6 +2,25 @@
 
 #include "../Core.h"
 
+#if defined(_WIN32) || defined(_WIN64)
+
+#include <Windows.h>
+
+#ifndef THREAD_HANDLE
+#define THREAD_HANDLE HANDLE
+#endif
+
+#elif defined(__unix) || defined(__unix__)
+
+#elif  defined(__APPLE__) || defined(__MACH__)
+
+#else
+static_assert(false);
+#endif
+
+
+
+
 namespace doom
 {
 	/// <summary>
@@ -24,7 +43,21 @@ namespace doom
 		/// sleep for milliseconds
 		/// </summary>
 		/// <param name="milliseconds">sleep time</param>
-		FORCE_INLINE static void _Sleep(unsigned long milliseconds);
+		FORCE_INLINE static void _Sleep(const unsigned long milliseconds);
+
+		/// <summary>
+		/// Processor Number of Current Thread
+		/// </summary>
+		/// <returns></returns>
+		FORCE_INLINE static unsigned int _GetCurrentProcessorNumber();
+		FORCE_INLINE static THREAD_HANDLE _GetCurrenThreadHandle();
+		FORCE_INLINE static unsigned int _GetCurrenThreadID();
+
+		/*
+		FORCE_INLINE static unsigned long long _SetThreadAffinity(const THREAD_HANDLE threadHandle, const unsigned long long threadAffinitMask);
+		
+		FORCE_INLINE static unsigned long long _GetThreadAffinity(const THREAD_HANDLE threadHandle);
+		*/
 	};
 }
 
--- Doom3/Source/Core/OS/Window.inl
+++ Doom3/Source/Core/OS/Window.inl
@@ -1,4 +1,4 @@
-#include <Windows.h>
+
 
 unsigned long long doom::OS::_GetTickCount()
 {
@@ -9,7 +9,39 @@ unsigned long long doom::OS::_GetTickCount()
 #endif
 }
 
-void doom::OS::_Sleep(unsigned long milliseconds)
+void doom::OS::_Sleep(const unsigned long milliseconds)
 {
 	Sleep(milliseconds);
 }
+
+unsigned int doom::OS::_GetCurrentProcessorNumber()
+{
+	return GetCurrentProcessorNumber();
+}
+
+THREAD_HANDLE doom::OS::_GetCurrenThreadHandle()
+{
+	return GetCurrentThread();
+}
+
+unsigned int doom::OS::_GetCurrenThreadID()
+{
+	return GetCurrentThreadId();
+}
+
+unsigned long long doom::OS::_SetThreadAffinity(const THREAD_HANDLE threadHandle, const unsigned long long threadAffinitMask)
+{
+	return SetThreadAffinityMask(threadHandle, threadAffinitMask);
+}
+
+/*
+unsigned long long doom::OS::_GetThreadAffinity(const THREAD_HANDLE threadHandle)
+{
+	return SetThreadAffinityMask(threadHandle, threadAffinitMask);
+}
+
+unsigned int doom::OS::_GetCurrentProcessorNumber()
+{
+	return GetCurrentProcessorNumber();
+}
+*/
