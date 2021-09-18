60fd0f34ab7e4850c6958e7681ff7e1970f6260c
SungJinKang <hour30000@gmail.com>
2021-09-18 17:16:23 +0900

    change std::cout to printf in Assert macros Add D_ASSERT_LOG

--- Doom3/Source/Core/Macros/Assert.h
+++ Doom3/Source/Core/Macros/Assert.h
@@ -10,23 +10,44 @@
 #endif
 #include <cassert>
 #ifndef NDEBUG
-#include <iostream>
+#include <cstdio>
 
 #ifndef D_ASSERT
+
 #   define D_ASSERT(condition) \
     do { \
         if (! (condition)) { \
-            std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
-                      << " line " << __LINE__ << ": " << std::endl; \
+            std::printf("Assertion `%s' failed in %s line %d :", #condition, __FILE__, __LINE__); \
+            std::fflush(stdout); \
             std::terminate(); \
         } \
     } while (false)
+
+#endif 
+
+#ifndef D_ASSERT_LOG
+
+#   define D_ASSERT_LOG(condition, MessageFormat, ...) \
+    do { \
+        if (! (condition)) { \
+            std::printf("Assertion `%s' failed ( ", #condition); \
+            std::printf(#MessageFormat, ##__VA_ARGS__); \
+            std::printf(" ) in %s line %d :", __FILE__, __LINE__); \
+            std::fflush(stdout); \
+            std::terminate(); \
+        } \
+    } while (false)
+
 #endif
 
 #else
 
 #ifndef D_ASSERT
 #define D_ASSERT(condition) do { } while (false)
+#endif
+
+#ifndef D_ASSERT_LOG
+#define D_ASSERT_LOG(condition, MessageFormat, ...) do { } while (false)
 #endif
 
 #endif
