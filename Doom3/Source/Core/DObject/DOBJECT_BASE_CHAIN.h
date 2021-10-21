a16b54ee48c17e266293e895eb3475bb96ef192b
sungJinKangdev <hour30000@gmail.com>
2021-10-21 11:26:25 +0900

    add some comment

--- Doom3/Source/Core/DObject/DOBJECT_BASE_CHAIN.h
+++ Doom3/Source/Core/DObject/DOBJECT_BASE_CHAIN.h
@@ -4,6 +4,9 @@
 
 #include "Macros/TypeDef.h"
 
+//BASE_CHAIN_COUNT가 아무리 컴파일 타임에 결정되어도 MAX_DOBJECT_BASE_CHAIN_DEPTH 값은 필요할 듯
+// DOBJECT_BASE_CHAIN에 템플릿을 붙여서 BASE_CHAIN_TYPE_ID_LIST 사이즈를 클래스 타입마다 유동적으로 만들려면 코드 너무 더러워진다.
+// 그냥 BASE_CHAIN_TYPE_ID_LIST의 array 길이는 내가 임의로 지정하자. 
 #define MAX_DOBJECT_BASE_CHAIN_DEPTH 15
 
 namespace doom
