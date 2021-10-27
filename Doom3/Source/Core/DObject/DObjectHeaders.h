800765f3e8136258e59b442ddd02e879ab0467c3
sungJinKangdev <hour30000@gmail.com>
2021-10-28 06:46:04 +0900

    add clReflect and clReflect_automation submodule

--- Doom3/Source/Core/DObject/DObjectHeaders.h
+++ Doom3/Source/Core/DObject/DObjectHeaders.h
@@ -1,5 +1,30 @@
 #pragma once
 
+#ifndef REFLECTION_ENABLED
+#define REFLECTION_ENABLED
+#endif
+
+#ifdef REFLECTION_ENABLED
+#include "Reflection/clReflect/inc/clcpp/clcpp.h"
+
+#ifndef DOBJECT_REFLECTIONS
+#define DOBJECT_REFLECTIONS(X) clcpp_reflect(X)
+#endif
+
+#else
+
+#ifndef DOBJECT_REFLECTIONS
+#define DOBJECT_REFLECTIONS(X)
+#endif
+
+#endif
+
+DOBJECT_REFLECTIONS(doom)
+namespace doom
+{
+	
+}
+
 #include <DObject/DObject.h>
 #include <DObject/DObjectManager.h>
 #include <DObject/DObjectGlobals.h>
