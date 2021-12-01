a46ed27bc4fb5e0da25738ee134e80f7aa89b31d
sungJinKangdev <hour30000@gmail.com>
2021-12-02 05:11:58 +0900

    add RootObjectLevel.h

---
+++ Doom3/Source/Core/GarbageCollector/RootObjectLevel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <TypeDef.h>
+
+namespace dooms
+{
+	inline extern const UINT32 ROOT_OBJECT_HIERARCHY_MAX_LEVEL = 3;
+	inline extern const UINT32 ROOT_OBJECT_HIERARCHY_MIN_LEVEL = 0;
+}
