8a035d5976a7200f0a294360d117548870ef9f22
sungJinKangdev <hour30000@gmail.com>
2021-12-02 01:03:52 +0900

    implement std::array reflection iterator.

--- Doom3/Source/Core/Reflection/TypeSpecialized/Helper/iteratorHelper.cpp
+++ Doom3/Source/Core/Reflection/TypeSpecialized/Helper/iteratorHelper.cpp
@@ -1,7 +1,72 @@
 #include "iteratorHelper.h"
 
+#include <array>
 #include <vector>
 
+
+dooms::reflection::Reflection_RandomAccessIterator dooms::reflection::helper::Generate_Reflection_RandomAccessIterator_FromStdArray
+(
+	void* const std_array_ptr,
+	const char* const typeFullName,
+	const DTemplateType& dTemplateType,
+	const size_t iteratorIndex
+)
+{
+	D_ASSERT(std_array_ptr != nullptr);
+	
+	const reflection::DTemplateArgumentType dTemplateTypeArgument = dTemplateType.GetTemplateArgumentType(0);
+
+	D_ASSERT(dTemplateTypeArgument.IsValid() == true);
+	
+	std::array<char, 1>* fakeArray = reinterpret_cast<std::array<char, 1>*>(std_array_ptr);
+
+	const size_t arrayElementTypeSize = (dTemplateTypeArgument.GetIsTemplateArgumentPointerType() == true) ? sizeof(void*) : dTemplateTypeArgument.GetTypeSize();
+	char* const ptr = reinterpret_cast<char*>(fakeArray->data()) + iteratorIndex * arrayElementTypeSize;
+
+	return Reflection_RandomAccessIterator{ ptr, arrayElementTypeSize };
+}
+
+dooms::reflection::Reflection_RandomAccessIterator dooms::reflection::helper::Generate_Reflection_RandomAccessIterator_FromStdArray\
+(
+	void* const std_array_ptr,
+	const char* const typeFullName,
+	const DTemplateType& dTemplateType,
+	const eIteratorIndex iteratorIndexType
+)
+{
+	D_ASSERT(std_array_ptr != nullptr);
+
+	const reflection::DTemplateArgumentType dTemplateTypeArgument = dTemplateType.GetTemplateArgumentType(0);
+
+	D_ASSERT(dTemplateTypeArgument.IsValid() == true);
+
+	std::array<char, 1>* fakeArray = reinterpret_cast<std::array<char, 1>*>(std_array_ptr);
+
+	const size_t arrayElementTypeSize = (dTemplateTypeArgument.GetIsTemplateArgumentPointerType() == true) ? sizeof(void*) : dTemplateTypeArgument.GetTypeSize();
+	char* ptr = reinterpret_cast<char*>(fakeArray->data());
+
+	if (iteratorIndexType == eIteratorIndex::End)
+	{
+		const std::string_view typeFullNameStringView = typeFullName;
+		const size_t lastTemplateArgumentComma = typeFullNameStringView.find_last_of(',');
+		const size_t lastTemplateCloseBlancket = typeFullNameStringView.find_last_of('>');
+
+		D_ASSERT(lastTemplateArgumentComma != std::string::npos && lastTemplateCloseBlancket != std::string::npos);
+
+		const std::string stdArraySizeTemplateArgumentString
+		{
+			typeFullNameStringView.begin() + lastTemplateArgumentComma + 1,
+			typeFullNameStringView.begin() + lastTemplateCloseBlancket
+		};
+
+		char* pos = NULL;
+		const size_t vectorElementRealCount = strtoll(stdArraySizeTemplateArgumentString.c_str(), &pos, 10);
+
+		ptr += vectorElementRealCount * arrayElementTypeSize;
+	}
+
+	return Reflection_RandomAccessIterator{ ptr, arrayElementTypeSize };
+}
 
 dooms::reflection::Reflection_RandomAccessIterator dooms::reflection::helper::Generate_Reflection_RandomAccessIterator_FromStdVector
 (
@@ -10,6 +75,8 @@ dooms::reflection::Reflection_RandomAccessIterator dooms::reflection::helper::Ge
 	const size_t iteratorIndex
 )
 {
+	D_ASSERT(std_vector_ptr != nullptr);
+
 	std::vector<char>* fakeStdVector = reinterpret_cast<std::vector<char>*>(std_vector_ptr);
 
 	const reflection::DTemplateArgumentType dTemplateTypeArgument = dTemplateType.GetTemplateArgumentType(0);
@@ -31,6 +98,8 @@ dooms::reflection::Reflection_RandomAccessIterator dooms::reflection::helper::Ge
 	const eIteratorIndex iteratorIndex
 )
 {
+	D_ASSERT(std_vector_ptr != nullptr);
+
 	std::vector<char>* fakeStdVector = reinterpret_cast<std::vector<char>*>(std_vector_ptr);
 
 	const reflection::DTemplateArgumentType dTemplateTypeArgument = dTemplateType.GetTemplateArgumentType(0);
--- Doom3/Source/Core/Reflection/TypeSpecialized/Helper/iteratorHelper.h
+++ Doom3/Source/Core/Reflection/TypeSpecialized/Helper/iteratorHelper.h
@@ -16,6 +16,38 @@ namespace dooms
 				End
 			};
 
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <param name="std_array_ptr"></param>
+			/// <param name="typeFullName">ex) std::array<int, 12></param>
+			/// <param name="dTemplateType"></param>
+			/// <param name="iteratorIndex"></param>
+			/// <returns></returns>
+			dooms::reflection::Reflection_RandomAccessIterator Generate_Reflection_RandomAccessIterator_FromStdArray
+			(
+				void* const std_array_ptr,
+				const char* const typeFullName,
+				const DTemplateType& dTemplateType,
+				const size_t iteratorIndex
+			);
+
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <param name="std_array_ptr"></param>
+			/// <param name="typeFullName">ex) std::array<int, 12></param>
+			/// <param name="dTemplateType"></param>
+			/// <param name="iteratorIndexType"></param>
+			/// <returns></returns>
+			dooms::reflection::Reflection_RandomAccessIterator Generate_Reflection_RandomAccessIterator_FromStdArray
+			(
+				void* const std_array_ptr,
+				const char* const typeFullName,
+				const DTemplateType& dTemplateType,
+				const eIteratorIndex iteratorIndexType
+			);
+
 			dooms::reflection::Reflection_RandomAccessIterator Generate_Reflection_RandomAccessIterator_FromStdVector
 			(
 				void* const std_vector_ptr,
