0a3b1784e0014091e2451a580c58a41160e698dc
SungJinKang <hour30000@gmail.com>
2021-12-21 05:07:14 +0900

    update some reflection.h files.

--- Doom3/Source/Core/Reflection/ReflectionType/DAttributeList.reflection.h
+++ Doom3/Source/Core/Reflection/ReflectionType/DAttributeList.reflection.h
@@ -1,16 +1,16 @@
 #pragma once
 
 // Utility Header File ( Don't Edit this )
-// SourceFilePath : C:\Doom3FromScratch\Doom3\Source\Core\Reflection\ReflectionType\DAttributeList.cpp
+// SourceFilePath : __Source_Core_Reflection_ReflectionType_DAttributeList_reflection_h
 
 
-#ifdef C__Doom3FromScratch_Doom3_Source_Core_Reflection_ReflectionType_DAttributeList_reflection_h
+#ifdef __Source_Core_Reflection_ReflectionType_DAttributeList_reflection_h
 
-#error "C:\Doom3FromScratch\Doom3\Source\Core\Reflection\ReflectionType\DAttributeList.reflection.h already included, missing '#pragma once' in C:\Doom3FromScratch\Doom3\Source\Core\Reflection\ReflectionType\DAttributeList.reflection.h"
+#error "__Source_Core_Reflection_ReflectionType_DAttributeList_reflection_h already included, missing '#pragma once' in __Source_Core_Reflection_ReflectionType_DAttributeList_reflection_h"
 
 #endif
 
-#define C__Doom3FromScratch_Doom3_Source_Core_Reflection_ReflectionType_DAttributeList_reflection_h
+#define __Source_Core_Reflection_ReflectionType_DAttributeList_reflection_h
 
 
 #include <type_traits>
--- Doom3/Source/Core/Reflection/ReflectionType/DTemplateType.reflection.h
+++ Doom3/Source/Core/Reflection/ReflectionType/DTemplateType.reflection.h
@@ -1,16 +1,16 @@
 #pragma once
 
 // Utility Header File ( Don't Edit this )
-// SourceFilePath : C:\Doom3FromScratch\Doom3\Source\Core\Reflection\ReflectionType\DTemplateType.cpp
+// SourceFilePath : __Source_Core_Reflection_ReflectionType_DTemplateType_reflection_h
 
 
-#ifdef C__Doom3FromScratch_Doom3_Source_Core_Reflection_ReflectionType_DTemplateType_reflection_h
+#ifdef __Source_Core_Reflection_ReflectionType_DTemplateType_reflection_h
 
-#error "C:\Doom3FromScratch\Doom3\Source\Core\Reflection\ReflectionType\DTemplateType.reflection.h already included, missing '#pragma once' in C:\Doom3FromScratch\Doom3\Source\Core\Reflection\ReflectionType\DTemplateType.reflection.h"
+#error "__Source_Core_Reflection_ReflectionType_DTemplateType_reflection_h already included, missing '#pragma once' in __Source_Core_Reflection_ReflectionType_DTemplateType_reflection_h"
 
 #endif
 
-#define C__Doom3FromScratch_Doom3_Source_Core_Reflection_ReflectionType_DTemplateType_reflection_h
+#define __Source_Core_Reflection_ReflectionType_DTemplateType_reflection_h
 
 
 #include <type_traits>
