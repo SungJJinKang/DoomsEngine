f3b162db27b5d3465bc34bd0058da7639c06f354
sungJinKangdev <hour30000@gmail.com>
2022-01-25 23:46:59 +0900

    add MemoryManager class, implemented MemoryManager::CheckIsIntelTBBLoaded function.

---
+++ Doom3/Source/Core/Memory/MemoryManager.cpp
@@ -0,0 +1,32 @@
+#include "MemoryManager.h"
+
+#include <oneapi/tbb/tbbmalloc_proxy.h>
+#include <EngineGUI/PrintText.h>
+
+void dooms::memory::MemoryManager::CheckIsIntelTBBLoaded() const
+{
+	char** tbbLog;
+	const int isTbbSuccess = TBB_malloc_replacement_log(&tbbLog);
+	D_ASSERT_LOG(isTbbSuccess == 0, *tbbLog);
+	if(isTbbSuccess == 0)
+	{
+		dooms::ui::PrintText("Intel TBB is successfully loaded ( %s )", *tbbLog);
+	}
+	else
+	{
+		dooms::ui::PrintText("Loading Intel TBB fail ( %s )", *tbbLog);
+	}
+}
+
+void dooms::memory::MemoryManager::Init()
+{
+	CheckIsIntelTBBLoaded();
+}
+
+void dooms::memory::MemoryManager::Update()
+{
+}
+
+void dooms::memory::MemoryManager::OnEndOfFrame()
+{
+}
