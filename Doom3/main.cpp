43f098812dd5887d6294a4e0f2244b77831e1a0b
sungJinKangdev <hour30000@gmail.com>
2022-01-25 22:54:04 +0900

    attach tbb malloc.

--- Doom3/main.cpp
+++ Doom3/main.cpp
@@ -1,7 +1,13 @@
 #include <GameEngineEntryPoint.h>
 
+#include <oneapi/tbb/tbbmalloc_proxy.h>
+
 int main()
 {
+	char** tbbLog;
+	const int isTbbSuccess = TBB_malloc_replacement_log(&tbbLog);
+	D_ASSERT_LOG(isTbbSuccess == 0, *tbbLog);
+
 	return dooms::GameEngineEntryPoint();
 }
 
