e8383139fb7e14043d9dff68ad118803e16084f8
SungJinKang <hour30000@gmail.com>
2022-02-15 17:09:54 +0900

    add some comments to meshBatchCreater.cpp

--- Doom3/Source/Engine/Rendering/Batch/BatchContainer/meshBatchCreater.cpp
+++ Doom3/Source/Engine/Rendering/Batch/BatchContainer/meshBatchCreater.cpp
@@ -1,9 +1,12 @@
 #include "meshBatchCreater.h"
 
-dooms::graphics::Mesh* dooms::graphics::meshBatchCreater::CreateStaticBatchedMesh(
+dooms::graphics::Mesh* dooms::graphics::meshBatchCreater::CreateStaticBatchedMesh
+(
 	const std::vector<dooms::Renderer*>& staticRendererList
 )
 {
 	// TODO : Implement this
+	// Store mesh vertex data in world space coordinate and TBN
+	// Check GbufferWriter_BatchedMesh_PBR file
 	return nullptr;
 }
