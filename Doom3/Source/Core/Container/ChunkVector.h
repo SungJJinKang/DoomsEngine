0fcb881ea4c88449ce5e9d0c3e74202cf2ebe83f
SungJinKang <hour30000@gmail.com>
2022-02-15 21:29:55 +0900

    add ChunkVector Container class, implementing it.

---
+++ Doom3/Source/Core/Container/ChunkVector.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <Core.h>
+
+namespace dooms
+{
+	/// <summary>
+	/// This library is for reducing reallocation overhead of std::vector when capacity is full
+	///
+	/// </summary>
+	/// <typeparam name="ELEMENT_TYPE"></typeparam>
+	template <typename ELEMENT_TYPE>
+	class D_CLASS ChunkVector
+	{
+
+	private:
+
+	public:
+
+	};
+}
+
