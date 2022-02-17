7b0fe38c9fd933d4ab03f40652d6a201bc12d938
sungJinKangdev <hour30000@gmail.com>
2022-02-18 05:51:12 +0900

    add mTBN to eVertexArrayFlag.

--- Doom3/Source/Engine/Rendering/Buffer/eVertexArrayFlag.h
+++ Doom3/Source/Engine/Rendering/Buffer/eVertexArrayFlag.h
@@ -9,12 +9,13 @@ namespace dooms
 		enum D_ENUM eVertexArrayFlag : UINT32
 		{
 			None = 0x0,
-			VertexVector3 = 1,
-			VertexVector2 = 2,
-			TexCoord = 4,
-			mNormal = 8,
-			mTangent = 16,
-			mBitangent = 32,
+			VertexVector3 = 1 << 0,
+			VertexVector2 = 1 << 1,
+			TexCoord = 1 << 2,
+			mNormal = 1 << 3,
+			mTangent = 1 << 4,
+			mBitangent = 1 << 5,
+			mTBN = 1 << 6,
 		};
 	}
 }
