5b6cf4df7d2af95bda2d352c2e363514c4a94d42
SungJinKang <hour30000@gmail.com>
2022-09-04 15:23:33 +0900

    remove some "inline const" from global variables...

--- Doom3/Source/Core/Graphics/Color.cpp
+++ Doom3/Source/Core/Graphics/Color.cpp
@@ -1,1 +1,43 @@
 #include "Color.h"
+
+
+const math::Vector4 dooms::graphics::Color::WHITE{ 1.0f, 1.0f, 1.0f, 1.0f };
+const math::Vector4 dooms::graphics::Color::BLACK{ 0.0f, 0.0f, 0.0f, 1.0f };
+const math::Vector4 dooms::graphics::Color::RED{ 1.0f, 0.0f, 0.0f, 1.0f };
+const math::Vector4 dooms::graphics::Color::GREEN{ 0.0f, 1.0f, 0.0f, 1.0f };
+const math::Vector4 dooms::graphics::Color::BLUE{ 0.0f, 0.0f, 1.0f, 1.0f };
+
+const math::Vector4& dooms::graphics::Color::GetColor(eColor color)
+{
+	switch (color)
+	{
+		case eColor::White:
+		{
+			return Color::WHITE;
+		}
+		case eColor::Black:
+		{
+			return Color::BLACK;
+		}
+		case eColor::Red:
+		{
+			return Color::RED;
+		}
+		case eColor::Green:
+		{
+			return Color::GREEN;
+		}
+		case eColor::Blue:
+		{
+			return Color::BLUE;
+		}
+		default:
+		{
+			NEVER_HAPPEN;
+			break;
+		}
+	}
+
+	return math::Vector4{ nullptr };
+}
+			
--- Doom3/Source/Core/Graphics/Color.h
+++ Doom3/Source/Core/Graphics/Color.h
@@ -20,58 +20,21 @@ namespace dooms
 			Blue = 4
 			
 		};
-
-		D_PROPERTY()
-		inline constexpr extern UINT32 ENUM_COLOR_COUNT = 5;
-
-		D_NAMESPACE(dooms::graphics::Color)
+		
+		enum
+		{
+			ENUM_COLOR_COUNT = 5
+		};
+		
 		namespace Color
 		{
-			D_PROPERTY()
-			inline extern const math::Vector4 WHITE{ 1.0f, 1.0f, 1.0f, 1.0f };
-
-			D_PROPERTY()
-			inline extern const math::Vector4 BLACK{ 0.0f, 0.0f, 0.0f, 1.0f };
-
-			D_PROPERTY()
-			inline extern const math::Vector4 RED{ 1.0f, 0.0f, 0.0f, 1.0f };
-
-			D_PROPERTY()
-			inline extern const math::Vector4 GREEN{ 0.0f, 1.0f, 0.0f, 1.0f };
-
-			D_PROPERTY()
-			inline extern const math::Vector4 BLUE{ 0.0f, 0.0f, 1.0f, 1.0f };
+			extern const math::Vector4 WHITE;
+			extern const math::Vector4 BLACK;
+			extern const math::Vector4 RED;
+			extern const math::Vector4 GREEN;
+			extern const math::Vector4 BLUE;
 		
-			inline extern const math::Vector4& GetColor(eColor color)
-			{
-				switch (color)
-				{
-					case eColor::White:
-						return Color::WHITE;
-					break;
-
-					case eColor::Black:
-						return Color::BLACK;
-						break;
-
-					case eColor::Red:
-						return Color::RED;
-						break;
-
-					case eColor::Green:
-						return Color::GREEN;
-						break;
-
-					case eColor::Blue:
-						return Color::BLUE;
-						break;
-
-					default:
-						__assume(0);
-						break;
-				}
-			}
-
+			extern const math::Vector4& GetColor(eColor color);
 		};
 		
 	}
