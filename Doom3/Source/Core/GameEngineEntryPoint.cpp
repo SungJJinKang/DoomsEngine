a48ba374569ffe0ddfd5ab67d8af044032ea8515
sungJinKangdev <hour30000@gmail.com>
2022-09-15 00:24:48 +0900

    add log about remained dobject count to where between game clean up and destroy all dobject function call

---
+++ Doom3/Source/Core/GameEngineEntryPoint.cpp
@@ -0,0 +1,59 @@
+#include "GameEngineEntryPoint.h"
+
+#include "Core.h"
+#include "Game/GameCore.h"
+
+#include <DObject/DObjectManager.h>
+#include <OS/ErrorHandling.h>
+#include <Graphics/GraphicsAPI/Manager/GraphicsAPIManager.h>
+#include "ResourceManagement/Thread/RunnableThread/GameThread.h"
+
+//void ExitGame();
+
+
+#include <iostream>
+#include <cstdio>
+
+int dooms::GameEngineEntryPoint(int argc, char* argv[])
+{
+	{
+		{
+			dooms::errorHandling::SetUnHandledExceptionHandling();
+
+			dooms::GameCore gameCore{};
+
+			D_START_PROFILING(Init_Game, dooms::profiler::eProfileLayers::CPU);
+			gameCore.Init(argc, argv);
+			D_END_PROFILING(Init_Game);
+
+			dooms::thread::GameThread::GetSingleton()->SetTickFunction
+			(
+				[&gameCore]()
+				{
+					return gameCore.Tick();
+				}
+			);
+
+			while (dooms::thread::GameThread::GetSingleton()->IsTerminated() == false)
+			{
+				dooms::thread::GameThread::GetSingleton()->Run_RunnableThread();
+			}
+
+			gameCore.CleanUp();
+			//window terminated
+		}
+
+		std::cout << "Remained DObject Count ( Before DestroyAllDObjects ) : " << dooms::DObjectManager::GetDObjectCount() << std::endl;
+
+		dooms::DObjectManager::DestroyAllDObjects(false); // for detecting memory leak
+
+	}
+
+	dooms::graphics::GraphicsAPIManager::DeInitialize();
+
+	D_ASSERT_LOG(dooms::DObjectManager::GetDObjectCount() == 0, "Never use DObject as static, global variable");
+	std::cout << "Remained DObject Count ( After DestroyAllDObjects )  : " << dooms::DObjectManager::GetDObjectCount() << std::endl;
+	std::fflush(stdout);
+
+	return 0;
+}
