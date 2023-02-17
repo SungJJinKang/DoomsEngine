7ee18de0c1fe2b0330c06159a4f3eb18ba8a4937
sungJinKangdev <hour30000@gmail.com>
2023-02-17 22:29:01 +0900

    add macros "D_DEBUG_CLOG", "D_RELEASE_LOG", "D_RELEASE_CLOG"

--- Doom3/Source/Core/Macros/Log.h
+++ Doom3/Source/Core/Macros/Log.h
@@ -1,19 +1,60 @@
 #pragma once
 
 #include <CompilerMacros.h>
-
-
-#ifdef DEBUG_MODE
 #include "../Logger/Logger.h"
+
+#ifndef ENABLE_DEBUG_LOG
+#define ENABLE_DEBUG_LOG DEBUG_MODE
+#endif
+
+#ifndef ENABLE_RELEASE_LOG
+#define ENABLE_RELEASE_LOG 1
+#endif
+
+
+
+#if ENABLE_DEBUG_LOG
 
 #ifndef D_DEBUG_LOG
 #define D_DEBUG_LOG(LOG_TYPE, ...) dooms::logger::Log(__FILE__, __LINE__, LOG_TYPE, ##__VA_ARGS__)
 #endif
 
+#ifndef D_DEBUG_CLOG
+#define D_DEBUG_CLOG(CONDITION, LOG_TYPE, ...) if(CONDITION) dooms::logger::Log(__FILE__, __LINE__, LOG_TYPE, ##__VA_ARGS__)
+#endif
+
 #else
 
 #ifndef D_DEBUG_LOG
-#define D_DEBUG_LOG(...)
+#define D_DEBUG_LOG(...) do { } while (false)
+#endif
+
+#ifndef D_DEBUG_CLOG
+#define D_DEBUG_CLOG(CONDITION, LOG_TYPE, ...) do { } while (false)
+#endif
+
+#endif
+
+
+
+#if ENABLE_RELEASE_LOG
+
+#ifndef D_RELEASE_LOG
+#define D_RELEASE_LOG(LOG_TYPE, ...) dooms::logger::Log(__FILE__, __LINE__, LOG_TYPE, ##__VA_ARGS__)
+#endif
+
+#ifndef D_RELEASE_CLOG
+#define D_RELEASE_CLOG(CONDITION, LOG_TYPE, ...) if(CONDITION) dooms::logger::Log(__FILE__, __LINE__, LOG_TYPE, ##__VA_ARGS__)
+#endif
+
+#else
+
+#ifndef D_RELEASE_LOG
+#define D_RELEASE_LOG(LOG_TYPE, ...) do { } while (false)
+#endif
+
+#ifndef D_RELEASE_CLOG
+#define D_RELEASE_CLOG(CONDITION, LOG_TYPE, ...) do { } while (false)
 #endif
 
 #endif
