573f15a3ddf9f5396b7c2760ca088b2754ba2a9b
SungJinKang <hour30000@gmail.com>
2023-02-19 17:31:54 +0900

    check "OVERDRAW_VISUALIZATION" before create instance of "OverDrawVisualization" class

--- Doom3/Source/Engine/Rendering/RenderingDebugger/RenderingDebuggerHelper.cpp
+++ Doom3/Source/Engine/Rendering/RenderingDebugger/RenderingDebuggerHelper.cpp
@@ -5,10 +5,12 @@
 #include "RenderingDebuggerModules/Modules/OverDrawVisualization.h"
 #include "RenderingDebuggerModules/Modules/RendererAABBDebugger.h"
 
+#include <EngineConfigurationData/ConfigData.h>
+
 std::vector<dooms::graphics::RenderingDebuggerModule*> dooms::graphics::renderingDebuggerHelper::CreateDefeaultRenderingDebuggerModules()
 {
 	std::vector<dooms::graphics::RenderingDebuggerModule*> renderingDebuggerModules{};
-	
+
 	renderingDebuggerModules.emplace_back
 	(
 		dooms::CreateDObject<EveryCullingProfilerDebugger>()
@@ -19,15 +21,18 @@ std::vector<dooms::graphics::RenderingDebuggerModule*> dooms::graphics::renderin
 		dooms::CreateDObject<MaskedOcclusionCullingTester>()
 	);
 
-	renderingDebuggerModules.emplace_back
-	(
-		dooms::CreateDObject<OverDrawVisualization>()
-	);
+	if (dooms::ConfigData::GetSingleton()->GetConfigData().GetValue<bool>("Graphics", "OVERDRAW_VISUALIZATION"))
+	{
+		renderingDebuggerModules.emplace_back
+		(
+			dooms::CreateDObject<OverDrawVisualization>()
+		);
+	}
 
 	renderingDebuggerModules.emplace_back
 	(
 		dooms::CreateDObject<RendererAABBDebugger>()
 	);
-	
+
 	return renderingDebuggerModules;
 }
